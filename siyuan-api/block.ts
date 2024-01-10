0451a86161ea064cc2cb6e97c5cd848a98c43cdf
dualwind <dualwind@qq.com>
2024-01-11 00:08:07 +0800

    getParentNextChildID 修改

--- siyuan-api/block.ts
+++ siyuan-api/block.ts
@@ -1,7 +1,14 @@
 import { BlockId } from "../types/siyuan-api";
 import { request } from "./common";
 
-export async function getParentNextChildID(id: BlockId): Promise<BlockId> {
+/**
+ * @description 返回的值不是父级的第一个子块，目前作用未知
+ * @param id 
+ * @returns 
+ */
+export async function getParentNextChildID(
+  id: BlockId
+): Promise<{ id: BlockId }> {
   return request("/api/block/getParentNextChildID", {
     id: id,
   });

a7526b7066022ff673ca5132f08cc024e5001d1d
dualwind <dualwind@qq.com>
2024-01-28 19:06:10 +0800

    .sy文件类型，api-insertBlock

--- siyuan-api/block.ts
+++ siyuan-api/block.ts
@@ -1,10 +1,24 @@
-import { BlockId } from "../types/siyuan-api";
+import { DocumentId, BlockId } from "../types/siyuan-api";
 import { request } from "./common";
+
+interface IResdoOperations {
+  doOperations: doOperation[];
+  undoOperations: doOperation[] | null;
+}
+
+type doOperation = {
+  action: string;
+  data: string;
+  id: BlockId;
+  parentID: BlockId | DocumentId;
+  previousID: BlockId;
+  retData: null;
+};
 
 /**
  * @description 返回的值不是父级的第一个子块，目前作用未知
- * @param id 
- * @returns 
+ * @param id
+ * @returns
  */
 export async function getParentNextChildID(
   id: BlockId
@@ -12,4 +26,25 @@ export async function getParentNextChildID(
   return request("/api/block/getParentNextChildID", {
     id: id,
   });
+}
+
+type DataType = "markdown" | "dom";
+export async function insertBlock(
+  dataType: DataType,
+  data: string,
+  nextID?: BlockId,
+  previousID?: BlockId,
+  parentID?: BlockId
+): Promise<IResdoOperations[]> {
+  if (!nextID && !previousID && !parentID) {
+    console.error(`insertBlock缺少参数id`);
+  }
+  let resData: { [idx: string]: any } = {
+    dataType: dataType,
+    data: data,
+  };
+  nextID ? (resData.nextID = nextID) : null;
+  previousID ? (resData.previousID = previousID) : null;
+  parentID ? (resData.parentID = parentID) : null;
+  return request("/api/block/insertBlock", resData);
 }
--- siyuan-api/common.ts
+++ siyuan-api/common.ts
@@ -2,6 +2,9 @@ import { IWebSocketData, fetchSyncPost } from "siyuan";
 
 export async function request(url: string, data: any) {
   let response: IWebSocketData = await fetchSyncPost(url, data);
+  if (response.code !== 0) {
+    console.warn(response);
+  }
   let res = response.code === 0 ? response.data : null;
   return res;
 }

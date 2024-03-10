d434b8b800c24a574c0b91dd3c47d2df7382ce68
dualwind <dualwind@qq.com>
2024-03-11 00:32:09 +0800

    fix(query): queryRefBlockById返回的是列表
    queryRefBlockById返回的是列表
    
    BREAKING CHANGE: queryRefBlockById返回的是列表，之前只返回第一个匹配的block

--- siyuan-api/outline.ts
+++ siyuan-api/outline.ts
@@ -1,19 +1,19 @@
-import { BlockTree, BlockId } from "../types/siyuan-api";
+import { BlockTree, BlockId, ETypeAbbrMap } from "../types/siyuan-api";
 import { request } from "./common";
 
 export type DocOutline = {
-  id?: string;
-  box?: string;
-  name?: string;
-  hPath?: string;
-  type?: string;
-  nodeType?: string;
-  subType?: string;
-  blocks?: Array<BlockTree>;
-  depth?: number;
-  count?: number;
-  updated?: string;
-  created?: string;
+  id: string;
+  box: string;
+  name: string;
+  hPath: string;
+  type: string;
+  nodeType: ETypeAbbrMap;
+  subType: string;
+  blocks: Array<BlockTree>;
+  depth: number;
+  count: number;
+  updated: string;
+  created: string;
 };
 export const getDocOutline = async (id: BlockId): Promise<DocOutline[]> => {
   return request("/api/outline/getDocOutline", {

04fc7c4bf051291a60f7b1486295772574df1572
dualwind <dualwind@qq.com>
2024-04-30 20:42:13 +0800

    fix(component\blockele): 更新buildBlock逻辑

--- component/blockEle.ts
+++ component/blockEle.ts
@@ -4,28 +4,29 @@ declare const Lute: typeof LuteClass;
 //* 可使用detail.protyle.lute.SpinBlockDOM(`<div>`)直接渲染
 //*详细信息见'app\src\protyle\hint\extend.ts'
 //*转换函数为 test\IHintData2Enum.js
-//* 不可见字符"\u200b" 及 Lute.Caret="‸"
+//* 不可见字符"\u200b" 及 Lute.Caret="Caret"
+const Caret = "Caret";
 export enum EHintType {
-  "模版" = "​",
-  "挂件" = "​1",
-  "资源" = "​2",
+  "模版" = "\u200b",
+  "挂件" = "\u200b1",
+  "资源" = "\u200b2",
   "引用块" = "((",
   "嵌入块" = "{{",
-  "ai chat" = "​5",
+  "ai chat" = "\u200b5",
   "数据库" = '<div data-type="NodeAttributeView" data-av-type="table"></div>',
-  "文档" = "​4",
-  "一级标题" = "# ‸",
-  "二级标题" = "## ‸",
-  "三级标题" = "### ‸",
-  "四级标题" = "#### ‸",
-  "五级标题" = "##### ‸",
-  "六级标题" = "###### ‸",
-  "无序列表" = "* ‸",
-  "有序列表" = "1. ‸",
-  "任务列表" = "* [ ] ‸",
-  "引述" = "> ‸",
+  "文档" = "\u200b4",
+  "一级标题" = "# " + Caret,
+  "二级标题" = "## " + Caret,
+  "三级标题" = "### " + Caret,
+  "四级标题" = "#### " + Caret,
+  "五级标题" = "##### " + Caret,
+  "六级标题" = "###### " + Caret,
+  "无序列表" = "* " + Caret,
+  "有序列表" = "1. " + Caret,
+  "任务列表" = "* [ ] " + Caret,
+  "引述" = "> " + Caret,
   "代码块" = "```",
-  "表格" = "| ‸ |  |  |\n| --- | --- | --- |\n|  |  |  |\n|  |  |  |",
+  "表格" = `| ${Caret} |  |  |\n| --- | --- | --- |\n|  |  |  |\n|  |  |  |`,
   "分割线" = "---",
   "数学公式块" = "$$",
   html = "<div>",
@@ -41,7 +42,7 @@ export enum EHintType {
   "标签" = "tag",
   "行内代码" = "code",
   "行内数学公式" = "inline-math",
-  "插入图片或文件" = "​3",
+  "插入图片或文件" = "\u200b3",
   "嵌入网址" = '<iframe sandbox="allow-forms allow-presentation allow-same-origin allow-scripts allow-modals" src="" border="0" frameborder="no" framespacing="0" allowfullscreen="true"></iframe>',
   "插入图片链接" = "![]()",
   "插入视频链接" = '<video controls="controls" src=""></video>',
@@ -52,11 +53,11 @@ export enum EHintType {
   "状态图" = "```graphviz\n```",
   "脑图" = "```mindmap\n```",
   "统一建模语言" = "```plantuml\n```",
-  "信息样式" = "style​color: var(--b3-card-info-color);background-color: var(--b3-card-info-background);",
-  "成功样式" = "style​color: var(--b3-card-success-color);background-color: var(--b3-card-success-background);",
-  "警告样式" = "style​color: var(--b3-card-warning-color);background-color: var(--b3-card-warning-background);",
-  "错误样式" = "style​color: var(--b3-card-error-color);background-color: var(--b3-card-error-background);",
-  "移除样式" = "style​",
+  "信息样式" = "style\u200bcolor: var(--b3-card-info-color);background-color: var(--b3-card-info-background);",
+  "成功样式" = "style\u200bcolor: var(--b3-card-success-color);background-color: var(--b3-card-success-background);",
+  "警告样式" = "style\u200bcolor: var(--b3-card-warning-color);background-color: var(--b3-card-warning-background);",
+  "错误样式" = "style\u200bcolor: var(--b3-card-error-color);background-color: var(--b3-card-error-background);",
+  "移除样式" = "style\u200b",
 }
 
 export const buildParaBlock = (html: string) => {
@@ -71,7 +72,7 @@ export const buildParaBlock = (html: string) => {
     <div contenteditable="true" spellcheck="false">
     ${html}
     </div>
-    <div class="protyle-attr" contenteditable="false">​</div>
+    <div class="protyle-attr" contenteditable="false">\u200b</div>
   </div>`;
   const child = div.firstChild as HTMLElement;
   return child;
@@ -92,20 +93,30 @@ export const buildSuperBlock = (layout: "col" | "row", htmls: string[]) => {
   data-sb-layout="${layout}"
 >
 ${htmls.reduce((pre, cur) => pre + cur, "")}
-<div class="protyle-attr" contenteditable="false">​</div>
+<div class="protyle-attr" contenteditable="false">\u200b</div>
 </div>`;
   const child = div.firstChild as HTMLElement;
   return child;
 };
 
-export const buildBlock = (type: EHintType, html: string, lute: LuteClass) => {
-  const blockHtml = lute.SpinBlockDOM(type);
+/**
+ *
+ * @param html
+ * @param lute 涉及到是否开关某些块
+ * @returns
+ */
+export const buildBlock = (html: string, lute: LuteClass, type?: EHintType) => {
   const div = document.createElement("div");
-  div.innerHTML = blockHtml;
+  let blockHtml;
   if (type === EHintType.html) {
+    blockHtml = lute.SpinBlockDOM(EHintType.html);
+    div.innerHTML = blockHtml;
     div
       .querySelector("protyle-html")
       .setAttribute("data-content", `<div>${html}</div>`);
+  } else {
+    blockHtml = lute.SpinBlockDOM(html);
+    div.innerHTML = blockHtml;
   }
   const child = div.firstChild as HTMLElement;
   return child;

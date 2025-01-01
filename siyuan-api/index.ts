3a3c44cb144108ee961f6aab01a3f2af0b501b58
dualwind <dualwind@qq.com>
2025-01-01 22:37:05 +0800

    feat: 统一导出siyuan-api

---
+++ siyuan-api/index.ts
@@ -0,0 +1,28 @@
+/* import * as attr from "./attr";
+import * as block from "./block";
+
+import * as file from "./file";
+import * as filetree from "./filetree";
+import * as outline from "./outline";
+import * as query from "./query";
+import * as search from "./search";
+import * as transaction from "./transaction";
+export const siyuanApi = {
+  attr,
+  block,
+  file,
+  filetree,
+  outline,
+  query,
+  search,
+  transaction,
+}; */
+export * from "./attr";
+export * from "./block";
+
+export * from "./file";
+export * from "./filetree";
+export * from "./outline";
+export * from "./query";
+export * from "./search";
+export * from "./transaction";

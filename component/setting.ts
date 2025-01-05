e3500067fad72f3b84171c3ea92dc731899ec0c2
dualwind <dualwind@qq.com>
2025-01-05 13:38:10 +0800

    feat(组件/setting): 支持滑块设置

--- component/setting.ts
+++ component/setting.ts
@@ -13,13 +13,41 @@ export const textEle = () => {
   return ele;
 };
 
+const sliderEle = (min: number, max: number, step: number) => {
+  const ele = document.createElement("input");
+  ele.className = "b3-slider fn__size200";
+  ele.type = "range";
+  ele.min = min.toString();
+  ele.max = max.toString();
+  ele.step = step.toString();
+  /*   ele.addEventListener("input", (e) => {
+    parent.setAttribute("aria-label", (e.target as HTMLInputElement).value);
+  }); */
+
+  return ele;
+};
+
+const sliderParent = (slider: HTMLInputElement) => {
+  const parent = document.createElement("div");
+  parent.className = "b3-tooltips b3-tooltips__n fn__flex-center";
+  //parent.setAttribute("aria-label", max.toString());
+  parent.appendChild(slider);
+  slider.addEventListener("mouseenter", (e) => {
+    parent.setAttribute("aria-label", (e.target as HTMLInputElement).value);
+  });
+  slider.addEventListener("mouseleave", (e) => {
+    parent.setAttribute("aria-label", (e.target as HTMLInputElement).value);
+  });
+  return parent;
+};
 /**
  * @param type "switch" | "input";
  */
 export type SettingData = {
-  type: string;
+  type: "switch" | "input" | "slider";
+  range?: { min: number; max: number; step: number }; //用于初始化slider的值
   title: string;
-  value: string | boolean;
+  value: string | boolean | number;
   description?: string;
 };
 
@@ -29,6 +57,7 @@ export const buildSetting = (
     storageName: string;
     isReload?: boolean;
     plugin: Plugin;
+    confirmCallback?: () => Promise<void>; //其他需要执行的回调
   }
 ): Setting => {
   const eleList: { [key: string]: HTMLInputElement } = {};
@@ -41,13 +70,19 @@ export const buildSetting = (
       case "input":
         ele = textEle();
         break;
+      case "slider": {
+        if (!data[key].range) throw new Error("slider need range");
+        const setValue = data[key].range;
+        ele = sliderEle(setValue.min, setValue.max, setValue.step);
+        break;
+      }
     }
     eleList[key] = ele;
   }
   const setting = new Setting({
     confirmCallback: async () => {
       for (const key of Object.keys(data)) {
-        let value: string | boolean;
+        let value: string | boolean | number;
         switch (data[key].type) {
           case "switch":
             value = eleList[key].checked;
@@ -55,13 +90,15 @@ export const buildSetting = (
           case "input":
             value = eleList[key].value;
             break;
+          case "slider":
+            value = parseInt(eleList[key].value);
+            break;
         }
         data[key].value = value;
       }
-      config.isReload
-        ? await config.plugin.saveData(config.storageName, data)
-        : null;
-      window.location.reload();
+      await config.plugin.saveData(config.storageName, data);
+      config.isReload ? window.location.reload() : null;
+      config.confirmCallback ? await config.confirmCallback() : null;
     },
   });
   for (const key of Object.keys(data)) {
@@ -76,6 +113,9 @@ export const buildSetting = (
           case "input":
             eleList[key].value = data[key].value as string;
             break;
+          case "slider":
+            eleList[key].value = String(data[key].value);
+            return sliderParent(eleList[key]);
         }
         return eleList[key];
       },
